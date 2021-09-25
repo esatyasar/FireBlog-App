d3b9dbbe8927a870627fa31c47fb0f0eec12aa2d
esatyasar <esatyasar91@gmail.com>
2021-09-25 23:46:30 +0300

    edited style

--- src/contexts/BlogContext.js
+++ src/contexts/BlogContext.js
@@ -8,9 +8,9 @@ function BlogProvider({children}) {
     const [counter,setCounter] = useState(1)
     const [NewBlog,setNewBlog] = useState({id:counter, title:"", image :"", content:""});
     const initialData = [{id : 1,
-                          title:"React Context",
-                          image: "https://picsum.photos/id/217/300/300",
-                          content: "lorem ipsum guy hukil asdilom asdiom"},
+        title:"React Context",
+        image: "https://picsum.photos/id/217/300/300",
+        content: "lorem ipsum guy hukil asdilom asdiom"},
                           { id: 2,
                             title:"React Redux",
                             image: "https://picsum.photos/id/89/300/300",
@@ -19,7 +19,6 @@ function BlogProvider({children}) {
     const [data,setData] = useState(initialData)
     const [details,setDetails] = useState({})
     
-
     const getDetails = (id) =>{
         const filtered = data.filter((item) =>{
             if (item.id === id){
