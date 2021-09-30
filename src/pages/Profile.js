cb8d1448387e1b91daf889912a4bdba073af7e4c
esatyasar <esatyasar91@gmail.com>
2021-09-30 16:46:46 +0300

    added user mail

--- src/pages/Profile.js
+++ src/pages/Profile.js
@@ -4,8 +4,10 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { Button, CardActionArea, CardActions } from '@mui/material';
+import { useAuth } from '../contexts/AuthContext'
 
 export default function Profile() {
+  const {currentUser} = useAuth()
   return (
     <Card  sx={{ maxWidth: 345, m:"auto", mt:10, width:340 }}>
       <CardActionArea >
@@ -26,7 +28,7 @@ export default function Profile() {
       </CardActionArea>
       <CardActions>
         <Button size="small" color="primary">
-          mail to : esatyasar91@gamil.com
+          mail to : {currentUser.email}
         </Button>
       </CardActions>
     </Card>
